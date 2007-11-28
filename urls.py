d2d58b87a4cb554c312afc3bf15874177dff270f
Jordi Funollet <jordi.f@ati.es>
2007-11-28 22:37:12 +0000

    homepage template is related to several apps, not just Txts. Moved up.

--- urls.py
+++ urls.py
@@ -11,11 +11,11 @@ urlpatterns = patterns('',
     (r'^r/', include('django.conf.urls.shortcut')),
     (r'^admin/', include('django.contrib.admin.urls')),
     #
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
     (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
     (r'^comments/', include('django.contrib.comments.urls.comments')),
     (r'^photoplanet/', include('photoplanet.urls')),
     (r'^preview/', 'misc.views.parse_markup'),
-    #(r'^links/', include('links.urls')),
     (r'^', include('txts.urls')),
 )
 
